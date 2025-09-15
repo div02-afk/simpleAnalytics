39c81cbafa4b6483e92b9811a365ddd66ae9f794
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2025-09-15 12:28:47 +0530

    feat: Add TenetService module with initial configuration and setup

---
+++ TenetService/src/main/java/com/simpleAnalytics/TenetService/TenetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simpleAnalytics.TenetService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenetServiceApplication.class, args);
+	}
+
+}
---
+++ TenetService/src/test/java/com/simpleAnalytics/TenetService/TenetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleAnalytics.TenetService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
